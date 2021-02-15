ca0b7a0996de595fed50d91f75221389c2410cc4
nikita.kozlov <nikita.kozlov@alseda.by>
2021-02-15 16:05:35 +0300

    Add date choice from DatePicker for report date

--- app/src/main/java/by/limitalltheir/keepersystem/report/ReportActivity.kt
+++ app/src/main/java/by/limitalltheir/keepersystem/report/ReportActivity.kt
@@ -1,14 +1,43 @@
 package by.limitalltheir.keepersystem.report
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
 import by.limitalltheir.keepersystem.R
+import com.afollestad.date.dayOfMonth
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.callbacks.onCancel
+import com.afollestad.materialdialogs.datetime.datePicker
 import kotlinx.android.synthetic.main.activity_report.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val TAG = "tag"
 
 class ReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+
+        val currentDate = Date()
+        val format = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        var dateText = format.format(currentDate)
+        date_tv.text = dateText
+
+        date_tv.setOnClickListener {
+            MaterialDialog(this)
+                .show {
+                    datePicker { dialog, date ->
+                        dateText = format.format(date.time)
+                        Log.d(TAG, dateText)
+                        dialog.cancelOnTouchOutside
+                    }
+                }
+                .positiveButton {
+                    date_tv.text = dateText ?: "ERROR"
+                }
+        }
 
         z_report_button.setOnClickListener {
 
